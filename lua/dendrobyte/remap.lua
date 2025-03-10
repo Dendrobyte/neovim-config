081701bcb4cb94e673f198221a4cb529aa20a680
Dendrobyte <markbacon78@gmail.com>
2025-03-09 17:25:22 -0700

    Scroll 4 lines at a time

--- lua/dendrobyte/remap.lua
+++ lua/dendrobyte/remap.lua
@@ -2,18 +2,21 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- Remap keys for window navigation
-vim.keymap.set("n", "<C-h>", "<C-w>h", { noremap = true})
-vim.keymap.set("n", "<C-j>", "<C-w>j", { noremap = true})
-vim.keymap.set("n", "<C-k>", "<C-w>j", { noremap = true})
-vim.keymap.set("n", "<C-l>", "<C-w>l", { noremap = true})
+vim.keymap.set("n", "<C-h>", "<C-w>h", { noremap = true })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { noremap = true })
+vim.keymap.set("n", "<C-k>", "<C-w>j", { noremap = true })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { noremap = true })
 
-vim.keymap.set("i", "<C-h>", "<C-w>h", { noremap = true})
-vim.keymap.set("i", "<C-j>", "<C-w>j", { noremap = true})
-vim.keymap.set("i", "<C-k>", "<C-w>j", { noremap = true})
-vim.keymap.set("i", "<C-l>", "<C-w>l", { noremap = true})
+vim.keymap.set("i", "<C-h>", "<C-w>h", { noremap = true })
+vim.keymap.set("i", "<C-j>", "<C-w>j", { noremap = true })
+vim.keymap.set("i", "<C-k>", "<C-w>j", { noremap = true })
+vim.keymap.set("i", "<C-l>", "<C-w>l", { noremap = true })
 
 -- Make the error lines show up
 vim.keymap.set("n", "<leader>se", vim.diagnostic.open_float, { noremap = true, silent = true })
+
+vim.keymap.set("n", "<C-e>", "4<C-e>", { noremap = true })
+vim.keymap.set("n", "<C-y>", "4<C-y>", { noremap = true })
 
 -- Setup harpoon stuff --
 -- Once again, could also be moved to a different file... --
